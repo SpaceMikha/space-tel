73242e2ce4f213793f79ca839dcc3148b9bbbc25
SpaceMikha <mikhaelmiro300@gmail.com>
2025-04-08 10:46:19 +0200

    add ddeltas to simulate drift

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,21 @@
 #include <chrono>
 
 int main() {
+
+    Telemetry state = {
+        420.0f, //alt
+        7.2f, //vel
+        100.0f, //bat
+        25.0f, //temp
+        0.0f, 0.0f, 0.0f //putch, yaw, roll
+    };
+
     while (true) {
-        Telemetry data = generateTelemetry();
+        Telemetry data = generateTelemetry(state);
         renderTelemetry(data);
 
-        // Wait for 1 second
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        // Wait for 10 seconds
+        std::this_thread::sleep_for(std::chrono::seconds(10));
     }
 
     return 0;
