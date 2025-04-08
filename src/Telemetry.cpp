759ba2c13837be54ff2a3773a6ab3b9e0501aacf
SpaceMikha <mikhaelmiro300@gmail.com>
2025-04-08 12:12:21 +0200

    create generate EPSData

--- src/Telemetry.cpp
+++ src/Telemetry.cpp
@@ -25,3 +25,32 @@ Telemetry generateTelemetry(const Telemetry& previous){
 
     return next;
 }
+
+EPSData generateEPSData(const EPSData& previous) {
+
+
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::uniform_real_distribution<float> delta(-0.2f, 0.2f);
+    std::uniform_real_distribution<float> solarInput(75.0f, 120.0f);
+
+    EPSData data;
+    data.voltage = std::fmax(0.0f, previous.voltage + delta(gen));
+    data.current = std::fmax(0.0f, previous.current + delta(gen));
+
+    //i am doing this way because I don't want to update C++ to version 17 xd
+    float nextCharge = previous.chargePercent + delta(gen);
+    if (nextCharge > 100.0f) nextCharge = 100.0f;
+    if (nextCharge < 0.0f)   nextCharge = 0.0f;
+    data.chargePercent = nextCharge;    data.solarInput = solarInput(gen);
+
+
+    // data.chargePercent = std::clamp(previous.chargePercent + delta(gen), 0.0f, 100.0f); <-- do this if you are using cpp v17
+
+
+    data.timeRemaining = std::fmax(0.0f, data.chargePercent / 1.5f); // basic estimate btw
+
+    return data;
+}
