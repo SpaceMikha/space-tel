8e3cfcd30da3ad2950161ea06ea84151785fcd0c
SpaceMikha <mikhaelmiro300@gmail.com>
2025-04-08 10:45:40 +0200

    generate deltas

--- src/Telemetry.cpp
+++ src/Telemetry.cpp
@@ -1,24 +1,27 @@
 #include "Telemetry.hpp"
 #include <random>
+#include <cmath>
 
-Telemetry generateTelemetry(){
+Telemetry generateTelemetry(const Telemetry& previous){
     std::random_device rd;
     std::mt19937 gen(rd());
 
-    std::uniform_real_distribution<float> altitude(400.0, 500.0);
-    std::uniform_real_distribution<float> velocity(400.0, 500.0);
-    std::uniform_real_distribution<float> battery(20.0, 100.0);
-    std::uniform_real_distribution<float> temperature(-20.0, 85.0);
-    std::uniform_real_distribution<float> orientation(-180.0, 180.0);
-
-    return Telemetry{
-        altitude(gen),
-        velocity(gen),
-        battery(gen),
-        temperature(gen),
-        orientation(gen),
-        orientation(gen),
-        orientation(gen),
-    };
     
+    std::uniform_real_distribution<float> delta_altitude(-1.0, 1.5);
+    std::uniform_real_distribution<float> delta_velocity(-0.1, 0.1);
+    std::uniform_real_distribution<float> delta_battery(-0.05, 0.005);
+    std::uniform_real_distribution<float> delta_temp(-0.5, 0.5);
+    std::uniform_real_distribution<float> delta_orientation(-2.0, 2.0);
+
+    Telemetry next;
+
+    next.altitude    = std::fmax(0.0f, previous.altitude + delta_altitude(gen));
+    next.velocity    = std::fmax(0.0f, previous.velocity + delta_velocity(gen));
+    next.battery     = std::fmax(0.0f, previous.battery + delta_battery(gen));
+    next.temperature = previous.temperature + delta_temp(gen);
+    next.pitch       = previous.pitch + delta_orientation(gen);
+    next.yaw         = previous.yaw + delta_orientation(gen);
+    next.roll        = previous.roll + delta_orientation(gen);
+
+    return next;
 }
